eb719555dbdcb4109eeb0ff2784e377cd0ad3129
netotz <neto.otz@hotmail.com>
2020-11-25 23:58:43 -0600

    Agregar opción generar mapa coroplético o de calor

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 '''
-Archivo para ejecutar programa.
+Archivo para ya sea generar mapas de calor de contaminantes
+o mapas coropléticos de ingresos por CIE.
 '''
 
-from heatmap import plot_heatmap
+# from heatmap import plot_heatmap
 
-pollutant = 'PM10'
-day = '1-Dec-17'
+# POLLUTANT = 'PM10'
+# DAY = '1-Dec-18'
 
-plot_heatmap(pollutant, day)
+# plot_heatmap(POLLUTANT, DAY)
+
+from choropleth import plot_entries_choropleth
+
+YEAR = 2018
+plot_entries_choropleth(YEAR)
