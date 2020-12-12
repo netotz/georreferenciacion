f31d76a994322289a67b2bf53cdc8959d132b393
netotz <neto.otz@hotmail.com>
2020-12-11 23:04:31 -0600

    Agrupar código de CLI en una función

--- cli.py
+++ cli.py
@@ -7,59 +7,63 @@ Funciones para la consola de comandos (CLI).
 import sys
 from argparse import ArgumentParser
 
-parser = ArgumentParser(
-    description='''
-        Genera un archivo HTML de un tipo de mapa especificado por
-        los siguientes argumentos.
-        Todos los mapas se grafican sobre el Área Metropolitana de Monterrey (AMM).
-        '''
-)
-subparsers = parser.add_subparsers(
-    title='tipo de mapa',
-    description='''
-        Define el tipo de mapa a generar.
-        Cada tipo tiene argumentos específicos:''',
-    required=True
-)
+def parse_arguments() -> None:
+    '''
+    Lee argumentos de la consola al usar el comando instalado.
+    '''
+    parser = ArgumentParser(
+        description='''
+            Genera un archivo HTML de un tipo de mapa especificado por
+            los siguientes argumentos.
+            Todos los mapas se grafican sobre el Área Metropolitana de Monterrey (AMM).
+            '''
+    )
+    subparsers = parser.add_subparsers(
+        title='tipo de mapa',
+        description='''
+            Define el tipo de mapa a generar.
+            Cada tipo tiene argumentos específicos:''',
+        required=True
+    )
 
-choropleth_parser = subparsers.add_parser(
-    'choroplethmap',
-    aliases=['cm'],
-    help='''
-        Mapa coroplético que muestra el conteo de casos de ingresos
-        agrupados por municipio, CIE y semana epidemiológica
-        de un año específico.'''
-)
-choropleth_parser.add_argument(
-    'year',
-    type=int,
-    help='Año del archivo de egresos a leer: EGRESOS_{year}.csv'
-)
+    choropleth_parser = subparsers.add_parser(
+        'choroplethmap',
+        aliases=['cm'],
+        help='''
+            Mapa coroplético que muestra el conteo de casos de ingresos
+            agrupados por municipio, CIE y semana epidemiológica
+            de un año específico.'''
+    )
+    choropleth_parser.add_argument(
+        'year',
+        type=int,
+        help='Año del archivo de egresos a leer: EGRESOS_{year}.csv'
+    )
 
-heat_parser = subparsers.add_parser(
-    'heatmap',
-    aliases=['hm'],
-    help='''
-        Mapa de calor que muestra la densidad de un contaminante
-        junto con marcadores que representan la velocidad y dirección
-        del viento de una fecha específica.'''
-)
-heat_parser.add_argument(
-    'pollutant',
-    choices='CO,NO,NO2,NOX,O3,PM10,PM2_5'.split(','),
-    help='Nombre del contaminante'
-)
-heat_parser.add_argument(
-    'date',
-    type=str,
-    help='''
-        Fecha en formato '<día>-<mes corto>-<año corto>',
-        ejemplo: '1-Dec-18'
-        '''
-)
+    heat_parser = subparsers.add_parser(
+        'heatmap',
+        aliases=['hm'],
+        help='''
+            Mapa de calor que muestra la densidad de un contaminante
+            junto con marcadores que representan la velocidad y dirección
+            del viento de una fecha específica.'''
+    )
+    heat_parser.add_argument(
+        'pollutant',
+        choices='CO,NO,NO2,NOX,O3,PM10,PM2_5'.split(','),
+        help='Nombre del contaminante'
+    )
+    heat_parser.add_argument(
+        'date',
+        type=str,
+        help='''
+            Fecha en formato '<día>-<mes corto>-<año corto>',
+            ejemplo: '1-Dec-18'
+            '''
+    )
 
-if len(sys.argv) == 1:
-    parser.print_help()
-    sys.exit(1)
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
 
-arguments = parser.parse_args()
+    arguments = parser.parse_args()
